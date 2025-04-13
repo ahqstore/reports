92380ecc449f8ecbef907e8b033999c8e7833021
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-04-13 11:41:14 +0530

    use defaults

--- issue.js
+++ issue.js
@@ -22,15 +22,33 @@ const repo = "reports";
 
 async function stuff() {
   const av = await new NodeClam().init({
-    debugMode: true,
-    scanRecursively: true,
-    clamdscan: {
-      reloadDb: true,
-      host: "localhost",
-      port: 3310,
-      active: true,
-      multiscan: true,
+    removeInfected: false, // If true, removes infected files
+    quarantineInfected: false, // False: Don't quarantine, Path: Moves files to this place.
+    scanLog: undefined, // Path to a writeable log file to write scan results into
+    debugMode: true, // Whether or not to log info/debug/error msgs to the console
+    fileList: undefined, // path to file containing list of files to scan (for scanFiles method)
+    scanRecursively: true, // If true, deep scan folders recursively
+    clamscan: {
+      path: "/usr/bin/clamscan", // Path to clamscan binary on your server
+      db: undefined, // Path to a custom virus definition database
+      scanArchives: true, // If true, scan archives (ex. zip, rar, tar, dmg, iso, etc...)
+      active: true, // If true, this module will consider using the clamscan binary
     },
+    clamdscan: {
+      socket: false, // Socket file for connecting via TCP
+      host: false, // IP of host to connect to TCP interface
+      port: false, // Port of host to use when connecting via TCP interface
+      timeout: 60000, // Timeout for scanning files
+      localFallback: true, // Use local preferred binary to scan if socket/tcp fails
+      path: "/usr/bin/clamdscan", // Path to the clamdscan binary on your server
+      configFile: undefined, // Specify config file if it's in an unusual place
+      multiscan: true, // Scan using all available cores! Yay!
+      reloadDb: true, // If true, will re-load the DB on every call (slow)
+      active: true, // If true, this module will consider using the clamdscan binary
+      bypassTest: false, // Check to see if socket is available when applicable
+      tls: false, // Use plaintext TCP to connect to clamd
+    },
+    preference: "clamdscan", // If clamdscan is found and active, it will be used by default
   });
 
   const ping = () => av.ping();
@@ -157,7 +175,7 @@ async function stuff() {
 
   // prettier-ignore
   const embed = new EmbedBuilder()
-    .setTitle(`Severity: ${isInfected ? "⚒️ Low" : "⚠️ Severe"}`)
+    .setTitle(`Severity: ${!isInfected ? "⚒️ Low" : "⚠️ Severe"}`)
     .setURL(url)
     .setFooter({
       text: "Report by AHQ Store Issues Bot",
@@ -198,7 +216,7 @@ async function stuff() {
    */
   const webhook = process.env.WEBHOOK || "";
 
-  const data = await fetch(webhook, {
+  await fetch(webhook, {
     method: "POST",
     body: JSON.stringify({
       content: isInfected ? `<@&1245401644733169724>` : `New Report`,
@@ -207,9 +225,7 @@ async function stuff() {
     headers: {
       "Content-Type": "application/json",
     },
-  }).then((d) => d.json());
-
-  console.log(data, webhook.length);
+  });
 }
 
 /**
@@ -240,7 +256,6 @@ async function download(file) {
       headers: {
         "User-Agent": "AHQ Store Issues Bot",
         "Authorization": `Bearer ${process.env.GITHUB_TOKEN}`,
-        "Content-Type": "application/json",
       },
     }).then((s) => s.data.pipe(f));
 
